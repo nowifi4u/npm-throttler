a612625c21a194541f7186b258022ddb75e7a43c
Maxim Shamber <36640388+nowifi4u@users.noreply.github.com>
2021-10-14 14:34:09 +0300

    feat: create Util.ts

---
+++ src/Util.ts
@@ -0,0 +1,26 @@
+export type Optional<T> = T | undefined | null;
+export interface EmptyInterface {}
+
+/**
+ * Parameter decorator for creating getter / setter
+ * with checker being called upon using setter
+ */
+export function GetSetCheck(checker: (v: unknown) => boolean, opts?: PropertyDescriptor) {
+  return (target: any, key: string | symbol) => {
+    let val = target[key];
+
+    const getter = () => val;
+    const setter = (v: unknown) => {
+      if (!checker(v)) throw new Error(`Invalid value being set to parameter "${String(key)}": ${String(v)}`);
+      val = v;
+    };
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+      enumerable: true,
+      configurable: true,
+      ...opts,
+    });
+  };
+}
